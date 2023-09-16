d4deb7e71f3154c41a1469de292466e63500f1e6
ljm <646505418@qq.com>
2023-09-16 17:08:13 +0800

    hackathon: init repo

---
+++ src/test/java/com/citi/hackathon/HackathonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citi.hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
